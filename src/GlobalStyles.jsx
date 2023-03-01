abc2b295c77057328ef9b3478140a06688275adb
Azucena Zhou <abbey-1825@hotmail.com>
2023-03-01 16:07:51 +0100

    add styled-components + google font

---
+++ src/GlobalStyles.jsx
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+    *,
+    *::before,
+    *::after {
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+    }
+
+    body {
+        font-family: 'Source Sans Pro', sans-serif;
+    }
+`;
+
+export default GlobalStyles;
